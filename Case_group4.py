e6f5841b06c97da2021fa8c578d70071a62183d3
ElinBee <elin.beetsma@gmail.com>
2024-10-10 17:26:57 +0200

    Add files via upload

--- Case_group4.py
+++ Case_group4.py
@@ -1,8 +1,5 @@
-import html
 import pandas as pd
 import streamlit as st
-import numpy as np
-import matplotlib.pyplot as plt
 import plotly.express as px
 import plotly.graph_objects as go
 import folium
